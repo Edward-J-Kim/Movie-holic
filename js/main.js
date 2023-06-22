9c97f50bd2dfc8314f263eb619fc77daa7f38b79
Edward Kim <ewakim90@gmail.com>
2023-06-22 18:26:14 +0000

    updated

--- js/main.js
+++ js/main.js
@@ -1,43 +1,14 @@
-// const $viewContainer = document.querySelector('#button-container');
-// const $buttons = document.querySelector('.button');
-// const $views = document.querySelector('.view');
+// const $homepage = document.querySelector('[data-view="homepage"]');
+// const $topAnime = document.querySelector('[data-view="topAnime"]');
+// const $table = document.querySelector('.table');
 
-// $viewContainer.addEventListener('click', handleViewSwap);
-// function handleViewSwap(event) {
-//   if (!event.target.matches('button')) {
-//     return;
+// function viewSwap(viewName) {
+//   if (viewName === 'homepage') {
+//     $homepage.classList.remove('hidden');
+//     $topAnime.classList.add('hidden');
+//   } else if (viewName === 'weaponsList') {
+//     $topAnime.classList.remove('hidden');
+//     $homepage.classList.add('hidden');
 //   }
-//   for (let buttonIndex = 0; buttonIndex < $buttons.length; buttonIndex++) {
-//     if ($buttons[buttonIndex] === event.target) {
-//       $buttons[buttonIndex].className = 'tab active';
-//     } else {
-//       $buttons[buttonIndex].className = 'tab';
-//     }
-//   }
-//   const viewName = event.target.getAttribute('data-view');
-//   console.log($views);
-//   for (let viewIndex = 0; viewIndex < $views.length; viewIndex++) {
-//     if ($views[viewIndex].getAttribute('data-view') !== viewName) {
-//       $views[viewIndex].className = 'view hidden';
-//     } else {
-//       $views[viewIndex].className = 'view';
-//     }
-//   }
+//   data.view = viewName;
 // }
-
-// document.addEventListener('DOMContentLoaded', function () {
-//   var xhr = new XMLHttpRequest();
-//   xhr.open('GET', 'https://api.jikan.moe/v4/top/anime');
-//   xhr.responseType = 'json';
-//   xhr.addEventListener('load', function () {
-
-//     const data = xhr.response;
-//     for (let i = 0; i < data.data.length; i++) {
-
-//       const topAnime = renderTopAnime(data.data[i]);
-//       $views.appendChild(topAnime);
-//     }
-//   });
-//   xhr.send();
-// }
-// );
